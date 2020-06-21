19d1ae71f4241a43de6248402bde28fb9a7c341f
bfkbx960 <bfkbx960@gmail.com>
2020-06-21 09:47:01 +0900

    lint fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,21 +3,21 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/prettier'],
   parserOptions: {
-    parser: "babel-eslint"
+    parser: 'babel-eslint'
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "prettier/prettier": [
-      "warn",
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'prettier/prettier': [
+      'warn',
       {
-        "#": "prettier config in here :)",
-        "singleQuote": true,
-        "semi": false,
-        "trailingComma": "none"
+        '#': 'prettier config in here :)',
+        singleQuote: true,
+        semi: false,
+        trailingComma: 'none'
       }
     ]
   }
-};
+}
